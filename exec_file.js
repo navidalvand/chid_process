0423c32919a96e36b3e51902a65509dc4f1932a2
navidalvand <navidalvand@gmail.com>
2024-02-27 01:14:40 +0330

    execFile

--- exec_file.js
+++ exec_file.js
@@ -1,8 +1,11 @@
-const { exec } = require("child_process");
+const { execFile } = require("child_process");
+const path = require("path");
 
-//?   the exec function runs a terminal command with nodejs and gives us some result
+//?     does the same shit but in a .sh file for bash scripting and bluh bluh bluh
 
-exec("node some.js & pwd", (err, stdout, stderr) => {
+const execPath = path.resolve(__dirname, "./execFile.sh");
+
+execFile(execPath, (err, stdout, stderr) => {
   if (err) return console.log(`err: ${err.message}`); //*   if ant errors in application would happen.
   if (stderr) return console.log(`stderr: ${stderr}`); //*    if any errors while running the command would happen.
   console.log(`stdout: ${stdout}`); //*       just to make sure the shit is working yk...
